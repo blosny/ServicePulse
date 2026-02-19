a234c6db20fc710b2c8863bdedabd9d01b974dd8
Taha Buğra Çiçek <tahabugracck@gmail.com>
2026-02-19 12:25:05 +0300

    feat: initial spring boot project with reactive web and docker support

---
+++ src/main/java/com/blosny/servicepulse/ServicePulseApplication.java
@@ -0,0 +1,13 @@
+package com.blosny.servicepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blosny/servicepulse/ServicePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blosny.servicepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
